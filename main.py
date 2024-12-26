8bbc50a63dfe992c87f0fff2e626c6b88f53fedc
Вадим Голов <grafit-design@inbox.ru>
2024-12-26 13:47:55 +0500

    Убрал почти все ограничения для реакции для лет и месяцев. Вынесу в отдельную функцию.

--- main.py
+++ main.py
@@ -10,29 +10,6 @@ from datetime import datetime
 from keyboards import start_keyboard, generate_calendar
 
 
-# await bot.send_chat_action(message.chat.id, 'Загрузка видео')
-# Удаление меню команд (если имеется)
-async def clear_commands(robot: Bot) -> None:
-    current_commands = await robot.get_my_commands()
-
-    if current_commands:
-        await robot.set_my_commands([])
-        await clear_commands(robot)
-
-
-# Команда /start
-@dp.message(Command('start'))
-async def start(message: Message):
-    await clear_commands(bot)
-
-    today = datetime.now()
-    now_year = today.year
-    now_month = today.month
-
-    await message.answer('Выберите дату:', reply_markup=start_keyboard)
-    await message.answer('Милый календарь v 1.0:', reply_markup=await generate_calendar(now_year, now_month, 'rus_short'))
-
-
 # Заглушка для ignore
 @dp.callback_query(F.data == 'ignore')
 async def ignore_callback(callback_query: CallbackQuery):
@@ -88,6 +65,31 @@ async def day_callback(callback_query: CallbackQuery):
 
     curr_day: int = int(callback_query.data.split('_')[1])
     await callback_query.answer(f'Вы выбрали: {curr_day} число')
+
+
+# await bot.send_chat_action(message.chat.id, 'Загрузка видео')
+# Удаление меню команд (если имеется)
+async def clear_commands(robot: Bot) -> None:
+    current_commands = await robot.get_my_commands()
+
+    if current_commands:
+        await robot.set_my_commands([])
+        await clear_commands(robot)
+
+
+# Команда /start
+@dp.message(Command('start'))
+async def start(message: Message):
+    await clear_commands(bot)
+
+    today = datetime.now()
+    now_year = today.year
+    now_month = today.month
+
+    options = load_init()
+
+    await message.answer('Выберите дату:', reply_markup=start_keyboard)
+    await message.answer('Милый календарь v 1.0:', reply_markup=await generate_calendar(now_year, now_month, options=options))
 
 
 # Запуск бота

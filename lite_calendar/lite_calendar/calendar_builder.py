7dbb70fc73fec3401810b4e37046ed752b45d330
Вадим Голов <grafit-design@inbox.ru>
2025-07-29 02:44:12 +0500

    дописал инициализацию глобальных приманных в picker_config.py, переписал __init__.py

--- lite_calendar/lite_calendar/calendar_builder.py
+++ lite_calendar/lite_calendar/calendar_builder.py
@@ -1,77 +1,44 @@
 import calendar
-from typing import Any
 from datetime import datetime
+from typing import Any
 
 from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from picker_config import *
 
-# 'EXT_MODE': EXT_MODE,
-# 'CONFIRM_BUTTON': CONFIRM_BUTTON,
-# 'START_DATE': START_DATE,
-# 'END_DATE': END_DATE,
-# 'DATE_FORMAT': DATE_FORMAT,
-# 'DAY_SET': DAY_SET,
-# 'MONTH_SET': MONTH_SET
 
-
-class AioBaseСalendar:
+class AioBaseCalendar:
     """
     Base class for calendar builders
 
     """
-    def __init__(self, current: datetime | None = None) -> None:
+    def __init__(self, current: datetime = datetime.now()) -> None:
         init_config()
 
-        self.builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+        self.builder = InlineKeyboardBuilder()
 
-        self.active_day: int
-        self.active_month: int
-        self.active_year: int
+        self.active_day: int = current.day
+        self.active_month: int = current.month
+        self.active_year: int = current.year
 
         self.current_year: int
         self.current_month: int
 
-        self.start_date: date
-        self.end_date: date
-        self.date_format: str
-        self.day_set: list[str]
-        self.month_set: list[str]
-        self.ext_mode: bool
-        self.confirm_button: bool
+        self.ext_mode: str = EXT_MODE
+        self.confirm_button: bool = CONFIRM_BUTTON.lower() == 'true'
+        self.start_date: str = START_DATE
+        self.end_date: str = END_DATE
+        self.date_format: str = DATE_FORMAT
+        self.day_set: list[str] = DAY_SET
+        self.month_set: list[str] = MONTH_SET
 
         self.callback_prefix: str
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-        self.today: datetime = datetime.today()
-        self.current: datetime = current or self.today
-
-
-        # Подтягиваем конфиг (можно сделать через init_config() -> CalendarConfig)
-        self.ext_mode: str = EXT_MODE
-        self.confirm_button: bool = True if CONFIRM_BUTTON == 'True' else False
-        self.start_date: datetime = START_DATE
-        self.end_date: datetime = END_DATE
-        self.date_format: str = DATE_FORMAT
-        self.day_set: list[str | int] = DAY_SET
-        self.month_set: list[str | int] = MONTH_SET
-
-
-class AioFullPicker(AioBaseСalendar):
+class AioFullPicker(AioBaseCalendar):
     pass
 
-class AioMiniPicker(AioBaseСalendar):
+
+class AioMiniPicker(AioBaseCalendar):
     pass

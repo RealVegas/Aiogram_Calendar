5af193bcbd1784d2db944f89f3057259198ffd92
Вадим Голов <grafit-design@inbox.ru>
2025-07-29 13:28:18 +0500

    Написал __init__ в AioBaseCalendar

--- lite_calendar/lite_calendar/calendar_builder.py
+++ lite_calendar/lite_calendar/calendar_builder.py
@@ -13,14 +13,9 @@ class AioBaseCalendar:
     Base class for calendar builders
 
     """
-    def __init__(self, now_date: datetime = datetime.now()) -> None:
+    def __init__(self) -> None:
         init_config()
-
-        self.builder = InlineKeyboardBuilder()
-
-        self.now_day: int = now_date.day
-        self.now_month: int = now_date.month
-        self.now_year: int = now_date.year
+        now_date: datetime = datetime.now()
 
         self.ext_mode: str = EXT_MODE
         self.confirm_button: bool = CONFIRM_BUTTON
@@ -30,11 +25,17 @@ class AioBaseCalendar:
         self.day_set: list[str] = DAY_SET
         self.month_set: list[str] = MONTH_SET
 
+        self.builder = InlineKeyboardBuilder()
+
+        self.now_day: int = now_date.day
+        self.now_month: int = now_date.month
+        self.now_year: int = now_date.year
+
         self.current_year: int = self.start_date.year
         self.current_month: int = self.start_date.month
         self.selected_date: datetime | None = None
 
-        self.callback_prefix: str
+        self.callback_prefix: str = 'litepick'
 
 
 class AioFullPicker(AioBaseCalendar):
